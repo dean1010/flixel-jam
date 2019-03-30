b5fc3bfcaca92ca89564cbf7e1dda2a7ac6a58a6
dean1010 <deanlifeson@yahoo.com>
2019-03-30 02:30:24 -0400

    Update TextEmitterFE.hx

--- jam/fx/frontends/TextEmitterFE.hx
+++ jam/fx/frontends/TextEmitterFE.hx
@@ -2,20 +2,12 @@ package jam.fx.frontends;
 
 import flixel.FlxObject;
 import flixel.effects.particles.FlxEmitter;
+import flixel.math.FlxPoint;
 import jam.fx.particles.TextEmitter;
 import jam.fx.particles.TextParticle;
-import jam.fx.FX;
 
-/**
- * FrontEnd for textEmitter.
- *
- * @author Dean Andreason
- */
 class TextEmitterFE
 {
-	/**
-	 * Function for emitting text particles.
-	 */
 	public function new() {}
 
 	/**
@@ -24,23 +16,36 @@ class TextEmitterFE
 	 * @param target      The FlxObject to focusOn and emit text particles.
 	 * @param text        The text string. Default is `?`.
 	 * @param quantity    How many text particles to emit. Default is `1`.
-	 * @param color       Optional color for the text particles.
-	 * @param index       Optional index color `FX.colors[index]` to use. Default is `-1`. (ignore)
+	 * @param color       Optional color for the text particles. Default is `0xFFFFFFFF` (white).
 	 * @param frequency   How often to emit a particle, if > 0. Default is `0` and sets `explode = true`.
-	 * @return This `FlxEmitter` instance (nice for chaining stuff together).
+	 * @param offset      Optional FlxPoint to offset the emitter.
+	 * @return This `FlxEmitter` instance.
 	 */
-	public function emit(emitter:TextEmitter, target:FlxObject, text:String = "?", quantity:Int = 1, color:Int = -1, index:Int = -1, frequency:Float = 0):FlxTypedEmitter<TextParticle>
+	public function emit(emitter:TextEmitter, target:FlxObject, text:String = "?", quantity:Int = 1, color:Int = 0xFFFFFFFF, frequency:Float = 0, ?offset:FlxPoint):FlxTypedEmitter<TextParticle>
 	{
-		if (index != -1)      emitter.color.set(FX.colors[index]);
-		else if (color != -1) emitter.color.set(color);
+		var explode = frequency > 0 ? false : true;
+
+		if (explode && emitter.emitting)
+		{
+			emitter.update(0);
+		}
+
+		if (color != 0xFFFFFFFF)
+		{
+			emitter.color.set(color);
+		}
 
 		emitter.text = text;
 
-		var explode = frequency > 0 ? false : true;
-
-		if (explode && emitter.emitting) emitter.update(0);
-
 		emitter.focusOn(target);
+
+		if (offset != null)
+		{
+			emitter.x += offset.x;
+			emitter.y += offset.y;
+			
+			offset.put();
+		}
 
 		return emitter.start(explode, frequency, quantity);
 	}
