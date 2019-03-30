3507eb2281b077e90c5cf59490be65dc03e13e34
dean1010 <deanlifeson@yahoo.com>
2019-03-30 02:29:42 -0400

    Update EmitterFE.hx

--- jam/fx/frontends/EmitterFE.hx
+++ jam/fx/frontends/EmitterFE.hx
@@ -3,16 +3,10 @@ package jam.fx.frontends;
 import flixel.FlxObject;
 import flixel.effects.particles.FlxEmitter;
 import flixel.effects.particles.FlxParticle;
-import jam.fx.FX;
+import flixel.math.FlxPoint;
 
-/**
- * FrontEnd for emitting particles.
- */
 class EmitterFE
 {
-	/**
-	 * Function for emitting particles.
-	 */
 	public function new() {}
 
 	/**
@@ -20,17 +14,13 @@ class EmitterFE
 	 * @param emitter     The emitter to emit.
 	 * @param target      The FlxObject to focusOn and emit particles.
 	 * @param quantity    How many particles to emit. Default is `0` and will emit all the particles.
-	 * @param color       Optional color to tint the particles.
-	 * @param index       Optional index color `FX.colors[index]` to use. Default is `-1`. (ignore)
+	 * @param color       Optional color for the particles. Default is `0x0` (transparent).
 	 * @param frequency   How often to emit a particle, if > 0. Default is `0` and sets `explode = true`.
+	 * @param offset      Optional FlxPoint to offset the emitter.
 	 * @return This `FlxEmitter` instance.
 	 */
-	public function emit(emitter:FlxEmitter, target:FlxObject, quantity:Int = 0, color:Int = -1, index:Int = -1, frequency:Float = 0):FlxTypedEmitter<FlxParticle>
+	public function emit(emitter:FlxEmitter, target:FlxObject, quantity:Int = 0, color:Int = 0x0, frequency:Float = 0, ?offset:FlxPoint):FlxTypedEmitter<FlxParticle>
 	{
-	//	if (index != -1)      emitter.color.set(FX.colors[index]);
-	//	else 
-		if (color != -1) emitter.color.set(color);
-
 		var explode = frequency > 0 ? false : true;
 
 		if (explode && emitter.emitting)
@@ -38,7 +28,20 @@ class EmitterFE
 			emitter.update(0);
 		}
 
+		if (color != 0x0)
+		{
+			emitter.color.set(color);
+		}
+
 		emitter.focusOn(target);
+
+		if (offset != null)
+		{
+			emitter.x += offset.x;
+			emitter.y += offset.y;
+			
+			offset.put();
+		}
 
 		return emitter.start(explode, frequency, quantity);
 	}
