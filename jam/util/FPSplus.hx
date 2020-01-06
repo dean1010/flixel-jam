47680fcb74340d314104c6abe3577a220d4dc63a
dean1010 <44176585+dean1010@users.noreply.github.com>
2020-01-06 17:42:33 -0500

    Fixed position and added delay timer

--- jam/util/FPSplus.hx
+++ jam/util/FPSplus.hx
@@ -11,6 +11,8 @@ import openfl.text.TextFormat;
 class FPSplus extends Sprite
 {
 	var times:Array<Float> = [];
+	var updateDelay:Float = 0.16;
+	var updateElapsed:Float = 0;
 	var textField:TextField;
 	var memPeak:Float = 0;
 	var showMem:Bool = true;
@@ -26,21 +28,25 @@ class FPSplus extends Sprite
 		this.showPeak = showPeak;
 
 		textField = new TextField();
-		textField.x = 4;
-		textField.y = 2;
+		textField.x = x + 2;
+		textField.y = y;
 		textField.selectable = false;
 		textField.defaultTextFormat = new TextFormat("_sans", textSize, textColor);
 		textField.autoSize = TextFieldAutoSize.LEFT;
 		textField.text = "FPS: 00";
+
 		if (showMem)  textField.text += "\nMEM: 100.0 MB";
-		if (showPeak) textField.text += "\npeak: 100.0 MB";
+		if (showPeak) textField.text += "\nPeak: 100.0 MB";
 
-		var w = textField.width + 8;
-		var h = textField.height + 4;
+		var w = x - textField.x + textField.width + 2;
+		var h = y - textField.y + textField.height + 2;
 
-		graphics.beginFill(bgColor, bgAlpha);
-		graphics.drawRect(x, y, w, h);
-		graphics.endFill();
+		if (bgAlpha > 0)
+		{
+			graphics.beginFill(bgColor, bgAlpha);
+			graphics.drawRect(x, y, w, h);
+			graphics.endFill();
+		}
 
 		addChild(textField);
 		addEventListener(Event.ENTER_FRAME, onEnter);
@@ -51,22 +57,33 @@ class FPSplus extends Sprite
 		if (!visible) return;
 
 		var now = Timer.stamp();
+		var elapsed = now - times[times.length - 1];
 
 		times.push(now);
 		while (times[0] < now - 1) times.shift();
-		textField.text = "FPS: " + times.length;
 
-		if (showMem || showPeak)
+		if (updateElapsed < updateDelay)
 		{
-			var mem = Math.round(System.totalMemory / 1024 / 1024 * 10) / 10;
+			updateElapsed += elapsed;
+		}
+		else
+		{
+			updateElapsed = 0;
 
-			if (mem > memPeak) memPeak = mem;
-			if (showMem) textField.text += "\nMEM: " + pad(mem) + " MB";
-			if (showPeak) textField.text += "\npeak: " + pad(memPeak) + " MB";
+			textField.text = "FPS: " + times.length;
+
+			if (showMem || showPeak)
+			{
+				var mem = Math.round(System.totalMemory / 1024 / 1024 * 10) / 10;
+
+				if (mem > memPeak) memPeak = mem;
+				if (showMem) textField.text += "\nMEM: " + dotZero(mem) + " MB";
+				if (showPeak) textField.text += "\nPeak: " + dotZero(memPeak) + " MB";
+			}
 		}
 	}
 
-	function pad(n:Float):String
+	function dotZero(n:Float):String
 	{
 		return (Std.string(n).indexOf(".") != -1 ? Std.string(n) : Std.string(n) + ".0");
 	}
